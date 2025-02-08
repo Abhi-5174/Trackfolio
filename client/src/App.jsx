b5f8728ca347c6f06c02013e4a81f5072c959c07
Dipali2377 <dipalim680@gmail.com>
2025-02-09 00:23:58 +0530

    profile and route changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 import ResetPassword from "./pages/ResetPassword";
 import Loading from "./components/Loading";
+import "./index.css";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -27,7 +29,6 @@ const App = () => {
 
     return () => clearTimeout(timer);
   }, []);
-
   if (loading) {
     return <Loading />;
   }
@@ -38,13 +39,48 @@ const App = () => {
       <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Home />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute>
+              <ForgotPassword />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/reset-password/:token"
+          element={
+            <PublicRoute>
+              <ResetPassword />
+            </PublicRoute>
+          }
+        />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/about" element={<About />} />
 
         {/* Private Routes */}
 
